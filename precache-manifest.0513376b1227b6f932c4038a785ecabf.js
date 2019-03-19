927d4da7083df347168e845ce690d4deb69ae268
Batman <lnmai@ucdavis.edu>
2019-03-19 00:26:45 -0700

    Updates

---
+++ precache-manifest.0513376b1227b6f932c4038a785ecabf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5c252a196be894f8b534047c82c75ec",
+    "url": "/static/media/logo.f5c252a1.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed8175a9165ba4db5b25",
+    "url": "/static/js/main.4b2c77a7.chunk.js"
+  },
+  {
+    "revision": "8f6f18fb074621dfc5ee",
+    "url": "/static/js/2.fadd7c56.chunk.js"
+  },
+  {
+    "revision": "ed8175a9165ba4db5b25",
+    "url": "/static/css/main.a931a6ba.chunk.css"
+  },
+  {
+    "revision": "a1c9c087bc539c30e10f885fd73f2e0b",
+    "url": "/index.html"
+  }
+];
