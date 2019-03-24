7633aba9d44cf389bc599787b8162af3990c53b9
Batman <lnmai@ucdavis.edu>
2019-03-24 02:27:24 -0700

    Updates

--- precache-manifest.de10f029c1317a5c9fdde50a6e0824d3.js
+++ precache-manifest.de10f029c1317a5c9fdde50a6e0824d3.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
-    "url": "/static/media/notification.c0d3c94c.eot"
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
   },
   {
-    "revision": "d94dbe72edafb48b8672",
-    "url": "/static/css/main.af0d59e9.chunk.css"
+    "revision": "249a134097e58c13e1cf",
+    "url": "/static/css/main.6168f122.chunk.css"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -16,16 +16,16 @@ self.__precacheManifest = [
     "url": "/static/media/digital-7 (mono).58045dab.ttf"
   },
   {
-    "revision": "b4483f7a53a93a42eacd",
-    "url": "/static/js/2.0e0ef9c9.chunk.js"
+    "revision": "6eb42d1283c040f4cb45",
+    "url": "/static/js/2.87848227.chunk.js"
   },
   {
-    "revision": "d94dbe72edafb48b8672",
-    "url": "/static/js/main.e3055b38.chunk.js"
+    "revision": "249a134097e58c13e1cf",
+    "url": "/static/js/main.70cdb09b.chunk.js"
   },
   {
-    "revision": "651771e1df95c807c99608188d0a4287",
-    "url": "/static/media/notification.651771e1.woff"
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
   },
   {
     "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "/static/media/notification.5bee74ca.svg"
   },
   {
-    "revision": "b4483f7a53a93a42eacd",
+    "revision": "6eb42d1283c040f4cb45",
     "url": "/static/css/2.993a9560.chunk.css"
   },
   {
-    "revision": "47bed99cce971ccb54dbccbe4a416b2d",
+    "revision": "1a8a77d9ceff5cc97f371aed51c03fe6",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71d285ef5df464686d0fc63253305cef.js"
+  "/precache-manifest.de10f029c1317a5c9fdde50a6e0824d3.js"
 );
 
 workbox.clientsClaim();
